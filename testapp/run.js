0f71a7e8d359221f5c2f9918c73741062d8575e3
Jim Coggeshall <jim@jimcoggeshall.com>
2019-09-18 18:50:20 +0000

    Add JSON extract to outputs

--- testapp/run.js
+++ testapp/run.js
@@ -16,6 +16,23 @@ const url = process.argv[2];
     const {data} = await session.send('Page.captureSnapshot');
     fs.writeFile('page.mhtml', data, 'utf8', () => {});
     await page.screenshot({path: 'screenshot.png', fullPage: true});
+    const extracted = await page.evaluate(() => {
+      const actualUrl = document.URL;
+      const links = Array.from(document.getElementsByTagName("a"));
+      return JSON.stringify(links.map((n) => {
+        return {
+          "actualUrl": actualUrl,
+          "href": n.getAttribute("href"),
+          "innerHTML": n.innerHTML,
+          "hreflang": n.getAttribute("hreflang"),
+          "ping": n.getAttribute("ping"),
+          "referrerpolicy": n.getAttribute("referrerpolicy"),
+          "rel": n.getAttribute("rel"),
+          "target": n.getAttribute("target")
+        };
+      }));
+    });
+    fs.writeFile('extracted.json', extracted,'utf8', () => {});
     await browser.close();
   } catch (e) {
     console.log(e.message);
