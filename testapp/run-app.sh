3830ae59397699e018f38a3f2364dc5539fa2e0d
Jim Coggeshall <jim@jimcoggeshall.com>
2020-09-16 11:50:04 -0700

    updates

--- testapp/run-app.sh
+++ testapp/run-app.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+rm -rf output
 mkdir -p output && chmod -R 777 output
 docker build -t testapp:testtag .
 docker-compose run --rm testapp
--- testapp/run.js
+++ testapp/run.js
@@ -2,8 +2,6 @@ const puppeteer = require('puppeteer-core');
 const axios = require('axios');
 const fs = require('fs');
 
-const url = process.argv[2];
-
 (async() => {
   try {
     const response = await axios.get('http://localhost:9222/json/version');
@@ -22,7 +20,21 @@ const url = process.argv[2];
       height: 1440,
       isLandscape: true
     });
-    page.goto(url, {timeout: 0});
+    await page.goto('https://enewspaper.sandiegouniontribune.com/desktop/sdut/default.aspx?pubid=ee84df93-f3c1-463c-a82f-1ab095a198ca', {waitUntil: 'networkidle2'});
+    let buttonId = await page.evaluate(() => {
+      let button = Array.from(document.querySelectorAll('p'))
+        .find(el => el.textContent === 'Continue');
+      let id = "randomidhopefullyunique";
+      button.setAttribute("id", id);
+      return id;
+    });
+    console.log("buttonId is " + buttonId);
+//    await page.waitForSelector('#ext-element-388 > #ext-container-111 > #ext-element-426 #ext-element-439')
+//    await page.click('#ext-element-388 > #ext-container-111 > #ext-element-426 #ext-element-439')
+//    await page.waitForSelector('#ext-element-388 > #ext-container-111 > #ext-element-426 #ext-element-439');
+//    await page.click('#ext-element-388 > #ext-container-111 > #ext-element-426 #ext-element-439');
+//    await page.waitForSelector('#ext-container-145 > #ext-element-511 > #ext-container-146 #ext-element-522');
+//    await page.click('#ext-container-145 > #ext-element-511 > #ext-container-146 #ext-element-522');
   } catch (e) {
     console.log(e.message);
     process.exit(1);
