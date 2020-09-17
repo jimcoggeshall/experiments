24e8e2ec8e4b7f695a1b4570ae0d98cd0dd143a0
Jim Coggeshall <jimcoggeshall@fico.com>
2020-09-16 18:52:44 -0700

    in progress

--- testapp/run.js
+++ testapp/run.js
@@ -9,32 +9,66 @@ const fs = require('fs');
     const browser = await puppeteer.connect({
       browserWSEndpoint: webSocketDebuggerUrl, 
       defaultViewport: {
-        width: 2304,
-        height: 1440,
+        width: 1365,
+        height: 729,
         isLandscape: true
       }
     });
     const page = await browser.newPage();
     page.setViewport({
-      width: 2304,
-      height: 1440,
+      width: 1365,
+      height: 729,
       isLandscape: true
     });
     await page.goto('https://enewspaper.sandiegouniontribune.com/desktop/sdut/default.aspx?pubid=ee84df93-f3c1-463c-a82f-1ab095a198ca', {waitUntil: 'networkidle2'});
-    let buttonId = await page.evaluate(() => {
+
+    await page.evaluate(() => {
       let button = Array.from(document.querySelectorAll('p'))
-        .find(el => el.textContent === 'Continue');
-      let id = "randomidhopefullyunique";
-      button.setAttribute("id", id);
-      return id;
+        .find(e => e.textContent === 'Continue');
+      let id = 'randomidhopefullyuniqueone';
+      button.setAttribute('id', id);
     });
-    console.log("buttonId is " + buttonId);
-//    await page.waitForSelector('#ext-element-388 > #ext-container-111 > #ext-element-426 #ext-element-439')
-//    await page.click('#ext-element-388 > #ext-container-111 > #ext-element-426 #ext-element-439')
-//    await page.waitForSelector('#ext-element-388 > #ext-container-111 > #ext-element-426 #ext-element-439');
-//    await page.click('#ext-element-388 > #ext-container-111 > #ext-element-426 #ext-element-439');
-//    await page.waitForSelector('#ext-container-145 > #ext-element-511 > #ext-container-146 #ext-element-522');
-//    await page.click('#ext-container-145 > #ext-element-511 > #ext-container-146 #ext-element-522');
+    await page.click('#randomidhopefullyuniqueone');
+    
+    await page.evaluate(() => {
+      let button = Array.from(document.querySelectorAll('.toolbar'))
+        .find(e => e.getAttribute('style').includes('menu-transparent'));
+      let id = 'randomidhopefullyuniquetwo';
+      button.setAttribute('id', id);
+    });
+    await page.click('#randomidhopefullyuniquetwo');
+
+    await page.evaluate(() => {
+      let button = Array.from(document.querySelectorAll('.x-innerhtml'))
+        .find(e => e.textContent === 'Download');
+      let id = 'randomidhopefullyuniquethree';
+      button.setAttribute('id', id);
+    });
+    await page.click('#randomidhopefullyuniquethree');
+    
+    await page.evaluate(() => {
+      let button = Array.from(document.querySelectorAll('.x-innerhtml'))
+        .find(e => e.textContent === 'Select All');
+      let id = 'randomidhopefullyuniquefour'
+      button.setAttribute('id', id);
+    });
+    await page.click('#randomidhopefullyuniquefour');
+
+    await page.evaluate(() => {
+      let button = Array.from(document.querySelectorAll('.x-innerhtml'))
+        .find(e => e.textContent === 'Download');
+      let id = 'randomidhopefullyuniquefive';
+      button.setAttribute('id', id);
+    });
+    await page.click('#randomidhopefullyuniquefive');
+
+    const session = await page.target().createCDPSession();
+    await session.send('Page.enable');
+    const {data} = await session.send('Page.captureSnapshot');
+    fs.writeFile('page.mhtml', data, 'utf8', () => {});
+
+    await browser.close();
+
   } catch (e) {
     console.log(e.message);
     process.exit(1);
