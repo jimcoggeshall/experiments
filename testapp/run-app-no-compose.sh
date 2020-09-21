99291d809ea5d04375d282f7ab88094d06c9edec
Jim Coggeshall <jim@jimcoggeshall.com>
2020-09-21 12:28:11 -0700

    add wrapper for old compose versions

---
+++ testapp/run-app-no-compose.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+rm -rf output
+mkdir -p output && chmod -R 777 output
+docker build -t testapp:testtag .
+docker run --shm-size='2gb' --network="host" -v ./output:/output --rm testapp:testtag
