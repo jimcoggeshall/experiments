e815d55f1f06e61153128cb1a46459ea21d21b82
Jim Coggeshall <jim@jimcoggeshall.com>
2020-09-21 11:29:22 -0700

    more general, less elegant

--- testapp/run.js
+++ testapp/run.js
@@ -9,56 +9,129 @@ const fs = require('fs');
     const browser = await puppeteer.connect({
       browserWSEndpoint: webSocketDebuggerUrl, 
       defaultViewport: {
-        width: 1365,
-        height: 729,
+        width:2304,
+        height: 1440,
         isLandscape: true
       }
     });
     const page = await browser.newPage();
+    await page.setDefaultTimeout(0);
     page.setViewport({
-      width: 1365,
-      height: 729,
+      width: 2304,
+      height: 1440,
       isLandscape: true
     });
-    await page.goto('https://enewspaper.sandiegouniontribune.com/desktop/sdut/default.aspx?pubid=ee84df93-f3c1-463c-a82f-1ab095a198ca', {waitUntil: 'networkidle2', timeout: 60000});
+    await page.goto('https://enewspaper.sandiegouniontribune.com/desktop/sdut/default.aspx?pubid=ee84df93-f3c1-463c-a82f-1ab095a198ca', {waitUntil: 'networkidle2', timeout: 0});
 
     await page.evaluate(() => {
-      while (true) {
+      var waitInterval = setInterval(() => {
         try {
           let button = Array.from(document.querySelectorAll('p'))
             .find(e => e.textContent === 'Continue');
+          let id = 'randomidhopefullyuniquezero';
+          button.setAttribute('id', id);
+          clearInterval(waitInterval);
+        } catch (e) {
+          () => {};
+        }
+      }, 1000);
+    });
+    await page.waitForSelector('#randomidhopefullyuniquezero');
+    await page.click('#randomidhopefullyuniquezero');
+
+    await page.evaluate(() => {
+      var waitInterval = setInterval(() => {
+        try {
+          let button = Array.from(document.querySelectorAll('x-inner'))
+            .find(e => {
+              e.parentNode.getAttribute('style').includes('downloads-transparent')
+            });
           let id = 'randomidhopefullyuniqueone';
           button.setAttribute('id', id);
-          break;
+          clearInterval(waitInterval);
         } catch (e) {
-          continue;
+          () => {};
         }
-      }
+      }, 1000);
     });
+    await page.waitForSelector('#randomidhopefullyuniqueone');
     await page.click('#randomidhopefullyuniqueone');
     
-    await page.waitForSelector('#ext-element-993', {timeout: 60000});
-    await page.click('#ext-element-993');
-    await page.waitForSelector('#ext-element-1137', {timeout: 60000});
-    await page.click('#ext-element-1137');
-    await page.waitForSelector('#ext-element-1143', {timeout: 60000});
-    await page.click('#ext-element-1143');
+    await page.evaluate(() => {
+      var waitInterval = setInterval(() => {
+        try {
+          let button = Array.from(document.querySelectorAll('x-innerhtml'))
+            .find(e => e.innerText === 'Select All');
+          let id = 'randomidhopefullyuniquetwo';
+          button.setAttribute('id', id);
+          clearInterval(waitInterval);
+        } catch (e) {
+          () => {};
+        }
+      }, 1000);
+    });
+    await page.waitForSelector('#randomidhopefullyuniquetwo');
+    await page.click('#randomidhopefullyuniquetwo');
+
+
+    await page.evaluate(() => {
+      var waitInterval = setInterval(() => {
+        try {
+          let button = Array.from(document.querySelectorAll('x-innerhtml'))
+            .find(e => e.innerText === 'Download');
+          let id = 'randomidhopefullyuniquethree';
+          button.setAttribute('id', id);
+          clearInterval(waitInterval);
+        } catch (e) {
+          () => {};
+        }
+      }, 1000);
+    });
+    await page.waitForSelector('#randomidhopefullyuniquethree');
+    await page.click('#randomidhopefullyuniquethree');
+
 
     const theLink = await page.evaluate(() => {
-      while (true) {
+      var waitInterval = setInterval(() => {
         try {
           let button = Array.from(document.querySelectorAll('a'))
             .find(e => e.href.split('.').pop() === 'pdf');
-          let id = 'randomidhopefullyuniquetwo';
+          let id = 'randomidhopefullyuniquefour';
           button.setAttribute('id', id);
+          clearInterval(waitInterval);
           return button.href;
         } catch (e) {
-          continue;
+          () => {};
         }
-      }
+      }, 1000);
     });
     console.log(theLink);
-    await page.click('#randomidhopefullyuniquetwo');
+    fs.writeFile('the_link.json', JSON.stringify({'link': theLink}), 'utf8', () => {});
+    await page.waitForSelector('#randomidhopefullyuniquefour');
+    await page.click('#randomidhopefullyuniquefour');
+
+    const session = await page.target().createCDPSession();
+    await session.send('Page.enable');
+    const {data} = await session.send('Page.captureSnapshot');
+    fs.writeFile('page.mhtml', data, 'utf8', () => {});
+    await page.screenshot({path: 'screenshot.png', fullPage: true});
+    const extracted = await page.evaluate(() => {
+      const actualUrl = document.URL;
+      const links = Array.from(document.getElementsByTagName("a"));
+      return JSON.stringify(links.map((n) => {
+        return {
+          "actualUrl": actualUrl,
+          "href": n.getAttribute("href"),
+          "innerHTML": n.innerHTML,
+          "hreflang": n.getAttribute("hreflang"),
+          "ping": n.getAttribute("ping"),
+          "referrerpolicy": n.getAttribute("referrerpolicy"),
+          "rel": n.getAttribute("rel"),
+          "target": n.getAttribute("target")
+        };
+      }));
+    });
+    fs.writeFile('extracted.json', extracted,'utf8', () => {});
 
     await browser.close();
 
