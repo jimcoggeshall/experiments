b6ff612d7d633ab556ff58a7e650e0057a1c5d82
Jim Coggeshall <jim@jimcoggeshall.com>
2020-09-23 09:15:31 -0700

    fix style function

--- testapp/run.js
+++ testapp/run.js
@@ -32,9 +32,11 @@ const fs = require('fs');
 
 
     await page.waitForSelector('.toolbarRightContainer > .x-inner > .toolbarIconRight');
-    const toolbarButtons = await page.$$( '.toolbarRightContainer > .x-inner > .toolbarIconRight');
+    const toolbarButtons = await page.$$('.toolbarRightContainer > .x-inner > .toolbarIconRight');
     const downloadButton = toolbarButtons.find(async button => {
-      let buttonStyle = await button.evaluate(e => e.getAttribute('style'));
+      let buttonStyle = await button.evaluate(e => {
+        window.getComputedStyle(e).getPropertyValue('background-image')
+      });
       return buttonStyle.includes('downloads-transparent');
     });
     await downloadButton.click();
